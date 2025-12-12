8e58b16b33025a287ab10a49b4ed03d3ae90e658
Priyanshu015211 <priyanshu0152@gmail.com>
2025-12-12 15:22:41 +0530

    POP UP CSS

--- background.js
+++ background.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 console.log("Background script is active");
+=======
+// Background service worker
+   console.log("LLM Safety Extension background loaded");
+>>>>>>> 0290880 (POP UP CSS)
--- content.js
+++ content.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 console.log("Content script injected");
+=======
+// Content script
+   console.log("LLM Safety content script loaded");
+>>>>>>> 0290880 (POP UP CSS)
